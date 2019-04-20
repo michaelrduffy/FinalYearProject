d885b98a348494d840f05480e749feb86034f4ee
Michael Duffy <duffymr@aston.ac.uk>
2019-04-20 12:03:43 +0100

    Fixed parameterised mutations

--- mutator.py
+++ mutator.py
@@ -71,6 +71,24 @@ def checkBrackets(robot):
         return numOpen - numClosed
     return 0
 
+#Return robot
+def split(s):
+ parts = []
+ bracket_level = 0
+ current = []
+ # trick to remove special-case of trailing chars
+ for c in (s + ","):
+     if c == "," and bracket_level == 0:
+         parts.append("".join(current))
+         current = []
+     else:
+         if c == "[":
+             bracket_level += 1
+         elif c == "]":
+             bracket_level -= 1
+         current.append(c)
+ return parts
+
 #Modify them
 def deserialise(robot):
     robotString = robot
@@ -80,29 +98,31 @@ def deserialise(robot):
     jointParams = ''
     children = ''
     skip = 0
-    if robotString[i+1] in PARAM_STARTS:
-        endChar = PARAM_ENDS[PARAM_STARTS.index(robotString[i+1])]
-        end = robotString.find(endChar, i+1)
-        if endChar == '}':
-            objParams = robotString[i+2:end]
-            skip = end
-        elif endChar == ')':
-            jointParams = robotString[i+2:end]
-            skip = end
-        i = skip
-        if(len(robotString)-1 > i+1):
-            if robotString[i+1] in PARAM_STARTS:
-                endChar = PARAM_ENDS[PARAM_STARTS.index(robotString[i+1])]
-                end = robotString.find(endChar, i+1)
-                if endChar == '}':
-                    objParams = robotString[i+2:end]
-                    skip = end
-                elif endChar == ')':
-                    jointParams = robotString[i+2:end]
-                    skip = end
-                i = skip
-    if robotString[i+1] == '[':
-        g.setChildren(robotString[i+2:-1])
+    if len(robotString) > 1:
+        if robotString[i+1] in PARAM_STARTS:
+            endChar = PARAM_ENDS[PARAM_STARTS.index(robotString[i+1])]
+            end = robotString.find(endChar, i+1)
+            if endChar == '}':
+                objParams = robotString[i+2:end]
+                skip = end
+            elif endChar == ')':
+                jointParams = robotString[i+2:end]
+                skip = end
+            i = skip
+            if(len(robotString)-1 > i+1):
+                if robotString[i+1] in PARAM_STARTS:
+                    endChar = PARAM_ENDS[PARAM_STARTS.index(robotString[i+1])]
+                    end = robotString.find(endChar, i+1)
+                    if endChar == '}':
+                        objParams = robotString[i+2:end]
+                        skip = end
+                    elif endChar == ')':
+                        jointParams = robotString[i+2:end]
+                        skip = end
+                    i = skip
+
+        if len(robotString)-1 > i+1 and robotString[i+1] == '[':
+            g.setChildren(robotString[i+2:-1])
     objParams = objParams.split(' ') if objParams != '' else None
     jointParams = jointParams.split(' ') if jointParams != '' else None
     g.setObjParams(objParams)
@@ -115,121 +135,52 @@ def deserialise(robot):
 def mutate(robot):
 
     if robot.objParams != None:
-        print 'Has objParams'
-        #Mutations Here
+        temp = robot.objParams
+        newParams = []
+        for p in temp:
+            foo = float(p)
+            multiplier = 1 + (random.sample([-1,1],1)[0] * random.uniform(0, 0.2))
+            newParams.append(str(foo * multiplier))
+        if len(temp) < 4 and random.randrange(100) > 60:
+            newParams.append(random.random())
+        robot.setObjParams(newParams)
+    elif random.randrange(100) > 60:
+        robot.setObjParams([random.uniform(0.1, 1)])
     if robot.jointParams != None:
-        print 'Has jointParams'
-        #Mutations Here
+        temp = robot.jointParams
+        newParams = []
+        for p in temp:
+            foo = float(p)
+            multiplier = 1 + (random.sample([-1,1],1)[0] * random.uniform(0, 0.2))
+            newParams.append(str(foo * multiplier))
+        if len(temp) < 4 and random.randrange(100) > 60:
+            newParams.append(random.random())
+        robot.setJointParams(newParams)
+    elif random.randrange(100) > 60:
+        robot.setJointParams([random.uniform(0.1, 1)])
     if robot.children != None:
         #Split children
         #Mutate Recursively
-        print 'children: ' + robot.childre
-
-        temp = deserialise(robot.children)
-        ch = robot.children[len(temp.toString()):]
-        
+        children = []
+        temp = robot.children
+        children = split(temp)
+        newChildren = []
+        for i, child in enumerate(children):
+            if child != '':
+                foo = deserialise(child)
+                bar = mutate(foo)
+                newChildren.append(bar)
+            else:
+                newChildren.append(',')
+        robot.setChildren(''.join(newChildren))
+    elif random.randrange(100) > 60:
+        robot.setChildren('F')
     return robot.toString()
 
-#Return robot
-
-testStr = 'F{6.0 3.2}(1.0)[F{0.1 0.0}(1.1)[F[F]],F[F{0.1}(1 2 3),,,F]]'
+testStr = 'F{6.0 3.2}(1.0)[F{0.1 0.0}(1.1)[F[F]],,,F[F{0.1}(1 2 3),,,F]]'
 genome = deserialise(testStr)
-mutate(genome)
+print mutate(genome)
 
-# def mutate(robot):
-#     # for i, char in enumerate(robot):
-#     #     if char == ',':
-#     #         if random.randrange(100) > 80:
-#     #             if random.randrange(100) > 95:
-#     #                 robot = robot[:i] + 'F' + robot[i+1:]
-#     #             else:
-#     #                 robot = robot[:i] + ',' + robot[i+1:]
-#     #
-#     #     if char == 'F' and i > 0:
-#     #         if random.randrange(100) > 90 and robot[i+1] != '[' :
-#     #             robot = robot[:i] + "F[]" + robot[i+1:]
-#     # for i in range(checkBrackets(robot)):
-#     #     robot += ']'
-#     subString = robot
-#     str_iter = enumerate(subString)
-#     newStr = ''
-#     for i, char in str_iter:
-#         if char not in BRANCH_TERMINATORS and char in ['F']:
-#             print char
-#             if char == ',':
-#                 if random.randrange(100) > 80:
-#                     subString[i] = "F"
-#                 continue
-#             objParams = ''
-#             jointParams = ''
-#             skip = 0
-#             if subString[i+1] in PARAM_STARTS:
-#                 endChar = PARAM_ENDS[PARAM_STARTS.index(subString[i+1])]
-#                 end = subString.find(endChar, i+1)
-#                 if endChar == '}':
-#                     objParams = subString[i+2:end]
-#                     skip = end
-#                 elif endChar == ')':
-#                     jointParams = subString[i+2:end]
-#                     skip = end
-#                 i = i+skip
-#                 if(len(subString)-1 > i+1):
-#                     if subString[i+1] in PARAM_STARTS:
-#                         endChar = PARAM_ENDS[PARAM_STARTS.index(subString[i+1])]
-#                         end = subString.find(endChar, i+1)
-#                         if endChar == '}':
-#                             objParams = subString[i+2:end]
-#                             skip = end
-#                         elif endChar == ')':
-#                             jointParams = subString[i+2:end]
-#                             skip = end
-#                         i = i+skip
-#
-#             # elif random.randrange(100) > 80:
-#             #     if random.randrange(100) > 90:
-#             #         subString = subString[:i] + "(0.0)" + subString[i+1:]
-#             #         skip = skip + 2
-#             #     else:
-#             #         subString = subString[:i] + "{0.0}" + subString[i+1:]
-#             #         skip = skip + 2
-#             objParams = objParams.split(' ') if objParams != '' else None
-#             jointParams = jointParams.split(' ') if jointParams != '' else None
-#             newGene = gene(char, objParams, jointParams)
-#             import pdb; pdb.set_trace()
-#             print newGene.toString()
-#             # if objParams != None and i != 0:
-#             #     for idx, p in enumerate(objParams):
-#             #         if p != '':
-#             #             if random.randrange(100) > 70:
-#             #                 mod = random.sample([-1,1], 1)[0] * (1.0 + random.uniform(-0.2, 0.2))
-#             #                 p = float(p) * mod
-#             #     if len(objParams) < 4:
-#             #         pString = ''
-#             #         objParams.append(0.0)
-#             #         objParams = [str(p) for p in objParams]
-#             #         pString = '{' + ' '.join(objParams) + '}'
-#             #
-#             #         subString = subString[:i-skip+1] + pString + subString[i-1:]
-#             # import pdb; pdb.set_trace()
-#             # if jointParams != None and i != 0:
-#             #     for idx, p in enumerate(jointParams):
-#             #         if p != '':
-#             #             if random.randrange(100) > 70:
-#             #                 mod = random.sample([-1,1], 1)[0] * (1.0 + random.uniform(-0.2, 0.2))
-#             #                 p = float(p) * mod
-#             #     if len(jointParams) < 5:
-#             #         pString = ''
-#             #         jointParams.append(0.0)
-#             #         jointParams = [str(p) for p in jointParams]
-#             #         pString = '(' + ' '.join(jointParams) + ')'
-#             #
-#             #         subString = subString[:i-skip+1] + pString + subString[i-1:]
-#             # for foo in range(skip):
-#             #     try:
-#             #         str_iter.next()
-#             #     except StopIteration:
-#             #         continue
-#     print subString
 #     robotDict = {"string": subString, "status":"todo"}
 #     # r.hmset(generateKey(), robotDict)
 #     return robot
